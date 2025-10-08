aa9f4cd676d6b37451a26ce5548a82e259b3ba98
user1 <sajjadahmedshaaz.dbpc@gmail.com>
2025-10-08 16:52:51 +0530

    completed q8

--- A3/p8.cpp
+++ A3/p8.cpp
@@ -1,8 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_SIZE 100
 
 class Stack {
-    
+    int arr[MAX_SIZE];
+    int top;
+
+    public:
+        Stack() {
+            top=-1;
+        }
+
+        void push(int x) {
+            if (top>=MAX_SIZE) {
+                cout << "Stack overflow";
+                return;
+            }
+            top=top+1;
+            arr[top]=x;
+        }
+
+        int pop() {
+            if (top<0) {
+                cout << "Stack underflow";
+                return -1;
+            }
+            int x=arr[top];
+            top=top-1;
+            return x;
+        }
+
+        bool isEmpty() {
+            return (top==-1);
+        }
 };
 
 int main() {
-    
+    Stack st;
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    while (!st.isEmpty()) {
+        cout << st.pop() << "\n";
+    }
 }
---
+++ A3/p9.cpp
@@ -0,0 +1,14 @@
+
+class Applicant {
+    static int cnt;
+
+    private:
+        int score;
+        char name[31];
+    
+    Applicant(char& name, int score) {
+        this->name=name;
+        this->score   =score
+
+    }
+};
