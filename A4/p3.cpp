677742ee7d896cb5808ff2106bc3b4e2b7ac081f
user1 <sajjadahmedshaaz.dbpc@gmail.com>
2025-10-29 16:48:54 +0530

    added a4 p3

--- A4/p3.cpp
+++ A4/p3.cpp
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 /*
 Design an ARRAY of integer class with the following features:
 a. Array object may be declared for a specific size and a value for initializing
@@ -13,5 +17,98 @@ iv. a*5 or 5*a will multiply the elements with 5.
 */
 
 class Array {
-    
+    private:
+        int size;
+        int* arr;
+
+    public:
+        Array(int size, int default_value=0) {
+            this->size=size;
+            // arr=(int *)malloc(size*default_value);
+            arr=new int[size];
+
+            for (int i=0; i<size; i++) {
+                arr[i]=default_value;
+            }
+        }
+
+        Array(const Array& other) {
+            size=other.size;
+            arr=new int[size];
+            
+            for (int i=0; i<size; i++) {
+                arr[i]=other.arr[i];
+            }
+        }
+
+        Array(int other[]) {
+            size=sizeof(other)/sizeof(int);
+            arr=new int[size];
+
+            for (int i=0; i<size; i++) {
+                arr[i]=other[i];
+            }
+        }
+
+        Array operator+(const Array& other) const {
+            if (size!=other.size) {
+                cout << "Error: Array size dont match!\n";
+                return *this;
+            }
+            Array temp(size);
+
+            for (int i=0; i<size; i++) {
+                temp.arr[i]=arr[i]+other.arr[i];
+            }
+            return temp;
+        }
+
+        Array operator*(int val) const {
+            Array temp(size);
+
+            for (int i=0; i<size; i++) {
+                temp.arr[i]=arr[i]*val;
+            }
+            return temp;
+        }
+
+        int& operator[](int index) {
+            if (index<0 || index>=size) {
+                cout << "index out of range";
+                exit(1);
+            }
+            return arr[index];
+        }
+
+        friend ostream& operator<<(ostream& os, Array& a) {
+            cout << "[";
+            for (int i=0; i<a.size; i++) {
+                cout << a.arr[i] << " ";
+            }
+            cout << "]";
+            cout << "\n";
+        }
+
+        friend Array operator*(int val, const Array& a) {
+            Array temp(a.size);
+
+            for (int i=0; i<a.size; i++) {
+                temp.arr[i]=a.arr[i]*val;
+            }
+            return temp;
+        }
+
+        // assignment operator overloading.....
+        // Array& operator=(const Array& other) {
+
+        // }
+};
+
+int main() {
+    Array a1(4, 5);
+    Array a2(4, 1);
+
+    Array a3=a1+a2;
+    a3=a3*5;
+    cout << a3;
 }
